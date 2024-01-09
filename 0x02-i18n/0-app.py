ee1051a9c5208e9f2f2aab3557be500996027e9d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-09 21:27:55 +0300

    Setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""Basic setup: Task 0"""
+"""The flask appp setup
+"""
 
 from flask import Flask, render_template
 
-app = Flask(__name___)
+app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    """this is the default route"""
-    return render_template("0-index.html")
+    """the default route"""
+    return render_template(
+        "0-index.html",
+    )
 
 
 if __name__ == "__main__":
